27c2f3aa31f0a2cc18717fc5f16ff1e916083e12
AislingMurphy1 <136448751+AislingMurphy1@users.noreply.github.com>
2023-06-19 14:52:53 +0100

    Made changes

--- app/src/main/java/com/example/homepage/map.java
+++ app/src/main/java/com/example/homepage/map.java
@@ -1,20 +1,14 @@
 package com.example.homepage;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class map extends AppCompatActivity {
 
