933242b3e82d4d5ca0174302b3245a53a8ee3ff3
MichaelaBrennan <michael.a.brennan@ericsson.com>
2023-06-19 13:35:55 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/homepage/MainActivity.java
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.homepage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton button;
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openNewActivity();
+            }
+        });
+        logout = findViewById(R.id.logoutBtn);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("remember", "false");
+                editor.apply();
+
+                finish();
             }
         });
     }
