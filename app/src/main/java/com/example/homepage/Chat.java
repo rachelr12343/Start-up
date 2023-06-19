37d656b6c10b4dabd42efaf3eece2993fcff73b3
rachelr12343 <136448873+rachelr12343@users.noreply.github.com>
2023-06-19 10:44:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/homepage/Chat.java
@@ -0,0 +1,81 @@
+package com.example.homepage;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chat extends AppCompatActivity {
+
+    private EditText messageEditText;
+    private Button sendButton;
+    private TextView chatTextView;
+
+    private StringBuilder chatHistory;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_button);
+        chatTextView = findViewById(R.id.chat_text_view);
+
+        chatHistory = new StringBuilder();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEditText.getText().toString();
+                sendMessage(message);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            String savedChatHistory = savedInstanceState.getString("chatHistory");
+            if (savedChatHistory != null) {
+                chatHistory = new StringBuilder(savedChatHistory);
+                chatTextView.setText(chatHistory.toString());
+            }
+        }
+    }
+
+    private void sendMessage(String message) {
+        String previousChat = chatTextView.getText().toString();
+        String newChat = previousChat + "\nYou: " + message;
+        chatTextView.setText(newChat);
+
+        chatHistory.append("You: ").append(message).append("\n");
+
+        // Simulating the other user's response
+        String response = generateResponse(message);
+        newChat = chatTextView.getText().toString() + "\nOther User: " + response;
+        chatTextView.setText(newChat);
+
+        chatHistory.append("Other User: ").append(response).append("\n");
+
+        // Clear the message EditText
+        messageEditText.getText().clear();
+    }
+
+    private String generateResponse(String message) {
+        // Replace this with your logic to generate the chatbot's response
+        return "This is the chatbot's response to: " + message;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("chatHistory", chatHistory.toString());
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(Chat.this, MainActivity.class);
+        startActivity(intent);
+}}
---
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (ImageButton) findViewById(R.id.chatbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(MainActivity.this, Chat.class);
+        startActivity(intent);
+    }
+}
