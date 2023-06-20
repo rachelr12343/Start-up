1ea78c3dab8a377d9e468141852c97cd2b92b67e
AislingMurphy1 <136448751+AislingMurphy1@users.noreply.github.com>
2023-06-20 13:40:02 +0100

    changes

--- app/src/main/java/com/example/homepage/menu.java
+++ app/src/main/java/com/example/homepage/menu.java
@@ -2,19 +2,26 @@ package com.example.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Map;
 
 public class menu extends AppCompatActivity {
+    ImageButton logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
-
-    public void openMainActivity(){
+    public void openMainActivity() {
         Intent intent = new Intent(menu.this, MainActivity.class);
         startActivity(intent);
-}}
+    }
+
+}
 
