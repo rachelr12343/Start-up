61f74e2f3da11e09ca3fbe07a5e2e2ea8f8370af
LeahTiernan <136448921+LeahTiernan@users.noreply.github.com>
2023-06-20 10:30:55 +0100

    working version

--- app/src/main/java/com/example/homepage/menu.java
+++ app/src/main/java/com/example/homepage/menu.java
@@ -2,6 +2,7 @@ package com.example.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class menu extends AppCompatActivity {
@@ -11,4 +12,7 @@ public class menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
-}
+    public void openMainActivity(){
+        Intent intent = new Intent(menu.this, MainActivity.class);
+        startActivity(intent);
+}}
