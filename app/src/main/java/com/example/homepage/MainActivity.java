31e093d44b1537d16e35f8387854301bfaac5b6d
AislingMurphy1 <136448751+AislingMurphy1@users.noreply.github.com>
2023-06-21 10:01:55 +0100

    fixed logout button

--- app/src/main/java/com/example/homepage/MainActivity.java
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Chat.class);
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);            }
 
             private void openNewActivity() {
