21b90a8e044010261f8808d34a9665361fe2675d
MichaelaBrennan <michael.a.brennan@ericsson.com>
2023-06-27 13:36:19 +0100

    Removed Unused Imports

--- app/src/main/java/com/example/homepage/MainActivity.java
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.homepage;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
