1d87943f419dbc2ecc354e5a6cd4d0f3537cabdd
MichaelaBrennan <michael.a.brennan@ericsson.com>
2023-06-27 11:02:53 +0100

    Fixes

--- app/src/main/java/com/example/homepage/Facilities.java
+++ app/src/main/java/com/example/homepage/Facilities.java
@@ -12,7 +12,6 @@ public class Facilities extends AppCompatActivity {
 
     ImageButton logo;
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
